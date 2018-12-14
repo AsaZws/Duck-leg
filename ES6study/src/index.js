dc62711a14653cad124b585918744c5964ed2e53
AsaZws <zhouws.com@gmail.com>
2018-12-14 17:56:39 +0800

    Promise

--- ES6study/src/index.js
+++ ES6study/src/index.js
@@ -341,19 +341,41 @@ console.log(pro.name);  // 技术胖
 
 */
 
-get = function () {
-    return 'I am JSPang';
-};
-var handler = {
-    apply(target, ctx, args) {
-        console.log('do apply');
-        return Reflect.apply(...arguments);
+let state = 1;
+function step1(resolve, reject) {
+    console.log('1.开始-洗菜做饭');
+    if(state == 1){
+        resolve('洗菜做饭--完成');
+    }else{
+        reject('洗菜做饭--出错');
     }
 }
-var pro = new Proxy(target, handler);
-console.log(pro());
-
-
+function step2(resolve,reject){
+    console.log('2.开始-坐下来吃饭');
+    if(state == 1){
+        resolve('坐下来吃饭--完成');
+    }else{
+        reject('坐下来吃饭--出错');
+    }
+}
+function step3(resolve,reject){
+    console.log('3.开始-收拾桌子洗碗');
+    if(state == 1){
+        resolve('收拾桌子洗碗--完成');
+    }else{
+        reject('收拾桌子洗碗--出错');
+    }
+}
+new Promise(step1).then(function(val){
+    console.log(val);
+    return new Promise(step2);
+}).then(function(val){
+    console.log(val);
+    return new Promise(step3);
+}).then(function(val){
+    console.log(val);
+    return val;
+});
 
 
 
