b1066bfdc143efde42b0fcec15f6ccb7999ef7da
AsaZws <zhouws.com@gmail.com>
2020-02-19 16:11:23 +0800

    mysql demo springBoot

---
+++ java/mysql-demo/src/main/java/com/example/mysqldemo/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.mysqldemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String hello() {
+
+        String sql = "SELECT mobile FROM user WHERE id = ?";
+
+        // 通过jdbcTemplate查询数据库
+        String mobile = (String)jdbcTemplate.queryForObject(
+                sql, new Object[] { 1 }, String.class);
+        return "查询到的数据是： " + mobile;
+
+    }
+
+}
